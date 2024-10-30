35b5f9ecea46958001025ea870a71a85ae4dbb72
harleensachdev <blackberezz@gmail.com>
2024-10-30 15:31:09 +0800

    updated

--- app.py
+++ app.py
@@ -147,7 +147,6 @@ def create_app():
             return redirect(url_for('login'))
         
         return render_template('register.html')
-
     @app.route("/login", methods=['GET', 'POST'])
     def login():
         if current_user.is_authenticated:
